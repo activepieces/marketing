afa5ad378393a92dd6d1dc4981d07103b547ca43
ashrafsamhouri <97393596+ashrafsamhouri@users.noreply.github.com>
2024-02-27 03:05:20 +0300

    Prevent homepage announcement from wrapping

--- components/pages/index/Hero.vue
+++ components/pages/index/Hero.vue
@@ -12,8 +12,8 @@ const { data: homepageAnnouncement } = await useFetch(`${config.public.strapiUrl
         <div class="lg:grid items-center gap-8 mb-16 lg:gap-12 lg:grid-cols-12">
             <div class="lg:col-span-6 text-center sm:mb-6 lg:text-left lg:mb-0">
                 <div class="max-w-screen-sm mx-auto">
-                    <a :href="homepageAnnouncement.data.attributes.url" v-if="homepageAnnouncement.data.attributes.content" class="inline-flex items-center justify-between px-1 py-1 pr-4 mb-6 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
-                        <span class="px-3 py-1 mr-3 text-xs text-white rounded-full bg-primary-600">New</span> <span class="text-sm font-medium">{{ homepageAnnouncement.data.attributes.content }}</span> 
+                    <a :href="homepageAnnouncement.data.attributes.url" v-if="homepageAnnouncement.data.attributes.content" class="inline-flex max-w-full items-center justify-between px-1 py-1 pr-4 mb-6 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+                        <span class="px-3 py-1 mr-3 text-xs text-white rounded-full bg-primary-600">New</span> <span class="text-sm font-medium overflow-hidden whitespace-nowrap text-ellipsis max-w-full">{{ homepageAnnouncement.data.attributes.content }}</span> 
                         <svg class="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                     </a>
                     <h1 class="mb-4 font-bold leading-none tracking-tight text-gray-900 text-5xl xl:text-6xl dark:text-white">Open source no-code business automation</h1>
