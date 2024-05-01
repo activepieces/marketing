c39c725ccd98e3d5c996d29b25ffab847529601d
ashrafsamhouri <97393596+ashrafsamhouri@users.noreply.github.com>
2024-05-01 12:56:59 +0300

    update social proofs

--- components/pages/index/Hero.vue
+++ components/pages/index/Hero.vue
@@ -17,7 +17,7 @@ const { data: homepageAnnouncement } = await useFetch(`${config.public.strapiUrl
                         <svg class="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                     </a>
                     <h1 class="mb-4 font-bold leading-none tracking-tight text-gray-900 text-5xl xl:text-6xl dark:text-white">Open source no-code business automation</h1>
-                    <p class="max-w-xl mx-auto mb-6 font-light text-gray-900 lg:mx-0 xl:mb-8 md:text-lg xl:text-xl dark:text-gray-400">Join over 30,000 users who are automating their internal marketing, sales and operations</p>
+                    <p class="max-w-xl mx-auto mb-6 font-light text-gray-900 lg:mx-0 xl:mb-8 md:text-lg xl:text-xl dark:text-gray-400">Join over 35,000 users who are automating their internal marketing, sales and operations</p>
                     <div class="flex flex-col space-y-4 sm:flex-row sm:justify-left sm:space-y-0 sm:space-x-4 justify-center lg:justify-start">
                         <a href="https://cloud.activepieces.com/sign-up" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                             Start free
