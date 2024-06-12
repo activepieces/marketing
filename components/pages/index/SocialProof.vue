6113df0279bf8dfc3afdba78e2f8867b7bcd54fe
ashrafsamhouri <97393596+ashrafsamhouri@users.noreply.github.com>
2024-06-13 01:15:02 +0300

    Update users to 40k

--- components/pages/index/SocialProof.vue
+++ components/pages/index/SocialProof.vue
@@ -36,7 +36,7 @@ defineProps({
       <div class="col-span-2 space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0">
         <div>
             <svg class="mb-2 w-8 h-8 text-fuchsia-400 mx-auto lg:mx-0" fill="currentColor" aria-hidden="true" viewBox="0 -960 960 960" xmlns="http://www.w3.org/2000/svg"><path d="m160-419 101-101-101-101L59-520l101 101Zm540-21 100-160 100 160H700Zm-220-40q-50 0-85-35t-35-85q0-51 35-85.5t85-34.5q51 0 85.5 34.5T600-600q0 50-34.5 85T480-480Zm0-160q-17 0-28.5 11.5T440-600q0 17 11.5 28.5T480-560q17 0 28.5-11.5T520-600q0-17-11.5-28.5T480-640Zm0 40ZM0-240v-63q0-44 44.5-70.5T160-400q13 0 25 .5t23 2.5q-14 20-21 43t-7 49v65H0Zm240 0v-65q0-65 66.5-105T480-450q108 0 174 40t66 105v65H240Zm560-160q72 0 116 26.5t44 70.5v63H780v-65q0-26-6.5-49T754-397q11-2 22.5-2.5t23.5-.5Zm-320 30q-57 0-102 15t-53 35h311q-9-20-53.5-35T480-370Zm0 50Z"/></svg>
-            <h3 class="mb-2 text-2xl font-bold dark:text-white">38k+ users</h3>
+            <h3 class="mb-2 text-2xl font-bold dark:text-white">40k+ users</h3>
             <p class="font-light text-gray-500 dark:text-gray-400">Giving continuous feedback to improve our tools</p>
         </div>
         <div>
