8cc0e33b5f8c6992922334bd2c1875f89ed752f9
ashrafsamhouri <97393596+ashrafsamhouri@users.noreply.github.com>
2024-06-23 02:01:24 +0300

    Change GTM to use official plugin + change social proofs

--- plugins/analytics.js
+++ plugins/analytics.js
@@ -1,22 +1,14 @@
-export default defineNuxtPlugin({
-  name: 'google-tag-manager',
-  setup(_, nuxtApp) {
-    const gtmId = 'GTM-TR47MDDQ';
+import { createGtm } from '@gtm-support/vue-gtm';
+import { useRouter } from 'vue-router';
 
-    if (process.client) {
-      (function(w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({
-          'gtm.start': new Date().getTime(),
-          event: 'gtm.js',
-        });
-        var f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s),
-          dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-      })(window, document, 'script', 'dataLayer', gtmId);
-    }
-  },
+export default defineNuxtPlugin((nuxtApp) => {
+  const router = useRouter();
+
+  const gtm = createGtm({
+    id: 'GTM-TR47MDDQ',
+    vueRouter: router,
+    debug: false,
+  });
+
+  nuxtApp.vueApp.use(gtm);
 });
