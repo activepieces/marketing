81d80e6268229aa157b43068331c727dd4b7e345
Abdullah Abu Sham <abusham_93@hotmail.com>
2024-08-12 03:23:20 +0300

    -finished departments section with responsive, left image

--- components/pages/index/Departments.vue
+++ components/pages/index/Departments.vue
@@ -1,11 +1,4 @@
-<script setup>
-// import { onMounted } from "vue";
-// import { initAccordions } from "flowbite";
-
-// onMounted(() => {
-//   initAccordions();
-// });
-</script>
+<script setup></script>
 
 <template>
   <section class="departments-section">
@@ -22,21 +15,118 @@
         solvers. Small enough to be simple and quick, but big enough to deliver
         the scope you want at the pace you need.
       </p>
-
-      <div class="flex flex-col gap-4 mt-8 sm:mt-12">
-        <div class="flex gap-x-4">
-          <!-- First row: first div takes up the space of exactly two normal divs including the gap -->
-          <div class="flex-grow border" style="flex-basis: calc(66.666% - 8px)">
-            Long div spanning two columns
+      <div class="flex flex-col gap-4 mt-8 sm:mt-12 text-left">
+        <div class="flex gap-x-6 max-[900px]:flex-col max-[900px]:gap-y-4">
+          <div class="flex-1 rounded-[16px] bg-white shadow-md p-6">
+            <div class="bg-primary-500 rounded-md w-fit h-fit p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                class="w-7 h-7"
+                fill="#FFF"
+              >
+                <path
+                  d="M480 32c0-12.9-7.8-24.6-19.8-29.6s-25.7-2.2-34.9 6.9L381.7 53c-48 48-113.1 75-181 75l-8.7 0-32 0-96 0c-35.3 0-64 28.7-64 64l0 96c0 35.3 28.7 64 64 64l0 128c0 17.7 14.3 32 32 32l64 0c17.7 0 32-14.3 32-32l0-128 8.7 0c67.9 0 133 27 181 75l43.6 43.6c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6l0-147.6c18.6-8.8 32-32.5 32-60.4s-13.4-51.6-32-60.4L480 32zm-64 76.7L416 240l0 131.3C357.2 317.8 280.5 288 200.7 288l-8.7 0 0-96 8.7 0c79.8 0 156.5-29.8 215.3-83.3z"
+                />
+              </svg>
+            </div>
+            <div class="max-w-[50%]">
+              <p class="text-gray-900 text-3xl font-semibold my-5">
+                Sales & Marketing
+              </p>
+              <p class="text-gray-700 text-xl leading-[28px]">
+                Lead follow-ups, email campaigns, social media scheduling, CRM
+                updates, lead scoring
+              </p>
+            </div>
           </div>
-          <div class="flex-1 border">Normal div</div>
+          <div
+            class="flex-1 rounded-[16px] bg-white shadow-md p-6 max-w-[400px] max-[900px]:flex-1 max-[900px]:max-w-none"
+          >
+            <div class="bg-primary-500 rounded-md w-fit h-fit p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                class="w-7 h-7"
+                fill="#FFF"
+              >
+                <path
+                  d="M96 0C60.7 0 32 28.7 32 64l0 384c0 35.3 28.7 64 64 64l288 0c35.3 0 64-28.7 64-64l0-384c0-35.3-28.7-64-64-64L96 0zM208 288l64 0c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zM512 80c0-8.8-7.2-16-16-16s-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16s16-7.2 16-16l0-64zM496 192c-8.8 0-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16s16-7.2 16-16l0-64c0-8.8-7.2-16-16-16zm16 144c0-8.8-7.2-16-16-16s-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16s16-7.2 16-16l0-64z"
+                />
+              </svg>
+            </div>
+            <p class="text-gray-900 text-3xl font-semibold my-5">HR</p>
+            <p class="text-gray-700 text-xl leading-[28px]">
+              Employee onboarding, document collection, training schedules
+            </p>
+          </div>
         </div>
-
-        <div class="flex gap-x-4">
-          <!-- Second row: three equal divs -->
-          <div class="flex-1 border">Div 1</div>
-          <div class="flex-1 border">Div 2</div>
-          <div class="flex-1 border">Div 3</div>
+        <div
+          class="flex gap-x-4 justify-between max-[900px]:flex-col max-[900px]:gap-y-4"
+        >
+          <div
+            class="flex-1 rounded-[16px] bg-white shadow-md p-6 max-w-[400px] max-[900px]:flex-1 max-[900px]:max-w-none"
+          >
+            <div class="bg-primary-500 rounded-md w-fit h-fit p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                class="w-7 h-7"
+                fill="#FFF"
+              >
+                <path
+                  d="M256 48C141.1 48 48 141.1 48 256l0 40c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-40C0 114.6 114.6 0 256 0S512 114.6 512 256l0 144.1c0 48.6-39.4 88-88.1 88L313.6 488c-8.3 14.3-23.8 24-41.6 24l-32 0c-26.5 0-48-21.5-48-48s21.5-48 48-48l32 0c17.8 0 33.3 9.7 41.6 24l110.4 .1c22.1 0 40-17.9 40-40L464 256c0-114.9-93.1-208-208-208zM144 208l16 0c17.7 0 32 14.3 32 32l0 112c0 17.7-14.3 32-32 32l-16 0c-35.3 0-64-28.7-64-64l0-48c0-35.3 28.7-64 64-64zm224 0c35.3 0 64 28.7 64 64l0 48c0 35.3-28.7 64-64 64l-16 0c-17.7 0-32-14.3-32-32l0-112c0-17.7 14.3-32 32-32l16 0z"
+                />
+              </svg>
+            </div>
+            <p class="text-gray-900 text-3xl font-semibold my-5">
+              Service Operations
+            </p>
+            <p class="text-gray-700 text-xl leading-[28px]">
+              Ticket assignment, response automation, customer feedback
+            </p>
+          </div>
+          <div
+            class="flex-1 rounded-[16px] bg-white shadow-md p-6 max-w-[400px] max-[900px]:flex-1 max-[900px]:max-w-none"
+          >
+            <div class="bg-primary-500 rounded-md w-fit h-fit p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                class="w-7 h-7"
+                fill="#FFF"
+              >
+                <path
+                  d="M320 96L192 96 144.6 24.9C137.5 14.2 145.1 0 157.9 0L354.1 0c12.8 0 20.4 14.2 13.3 24.9L320 96zM192 128l128 0c3.8 2.5 8.1 5.3 13 8.4C389.7 172.7 512 250.9 512 416c0 53-43 96-96 96L96 512c-53 0-96-43-96-96C0 250.9 122.3 172.7 179 136.4c0 0 0 0 0 0s0 0 0 0c4.8-3.1 9.2-5.9 13-8.4zm84 88c0-11-9-20-20-20s-20 9-20 20l0 14c-7.6 1.7-15.2 4.4-22.2 8.5c-13.9 8.3-25.9 22.8-25.8 43.9c.1 20.3 12 33.1 24.7 40.7c11 6.6 24.7 10.8 35.6 14l1.7 .5c12.6 3.8 21.8 6.8 28 10.7c5.1 3.2 5.8 5.4 5.9 8.2c.1 5-1.8 8-5.9 10.5c-5 3.1-12.9 5-21.4 4.7c-11.1-.4-21.5-3.9-35.1-8.5c-2.3-.8-4.7-1.6-7.2-2.4c-10.5-3.5-21.8 2.2-25.3 12.6s2.2 21.8 12.6 25.3c1.9 .6 4 1.3 6.1 2.1c0 0 0 0 0 0s0 0 0 0c8.3 2.9 17.9 6.2 28.2 8.4l0 14.6c0 11 9 20 20 20s20-9 20-20l0-13.8c8-1.7 16-4.5 23.2-9c14.3-8.9 25.1-24.1 24.8-45c-.3-20.3-11.7-33.4-24.6-41.6c-11.5-7.2-25.9-11.6-37.1-15c0 0 0 0 0 0l-.7-.2c-12.8-3.9-21.9-6.7-28.3-10.5c-5.2-3.1-5.3-4.9-5.3-6.7c0-3.7 1.4-6.5 6.2-9.3c5.4-3.2 13.6-5.1 21.5-5c9.6 .1 20.2 2.2 31.2 5.2c10.7 2.8 21.6-3.5 24.5-14.2s-3.5-21.6-14.2-24.5c-6.5-1.7-13.7-3.4-21.1-4.7l0-13.9z"
+                />
+              </svg>
+            </div>
+            <p class="text-gray-900 text-3xl font-semibold my-5">Finance</p>
+            <p class="text-gray-700 text-xl leading-[28px]">
+              Invoice processing, expense tracking, financial reporting, payroll
+              management
+            </p>
+          </div>
+          <div
+            class="flex-1 rounded-[16px] bg-white shadow-md p-6 max-w-[400px] max-[900px]:flex-1 max-[900px]:max-w-none"
+          >
+            <div class="bg-primary-500 rounded-md w-fit h-fit p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                class="w-7 h-7"
+                fill="#FFF"
+              >
+                <path
+                  d="M192 0c17.7 0 32 14.3 32 32l0 112-64 0 0-112c0-17.7 14.3-32 32-32zM64 64c0-17.7 14.3-32 32-32s32 14.3 32 32l0 80-64 0 0-80zm192 0c0-17.7 14.3-32 32-32s32 14.3 32 32l0 96c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-96zm96 64c0-17.7 14.3-32 32-32s32 14.3 32 32l0 64c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-64zm-96 88l0-.6c9.4 5.4 20.3 8.6 32 8.6c13.2 0 25.4-4 35.6-10.8c8.7 24.9 32.5 42.8 60.4 42.8c11.7 0 22.6-3.1 32-8.6l0 8.6c0 52.3-25.1 98.8-64 128l0 96c0 17.7-14.3 32-32 32l-160 0c-17.7 0-32-14.3-32-32l0-78.4c-17.3-7.9-33.2-18.8-46.9-32.5L69.5 357.5C45.5 333.5 32 300.9 32 267l0-27c0-35.3 28.7-64 64-64l88 0c22.1 0 40 17.9 40 40s-17.9 40-40 40l-56 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l56 0c39.8 0 72-32.2 72-72z"
+                />
+              </svg>
+            </div>
+            <p class="text-gray-900 text-3xl font-semibold my-5">Operations</p>
+            <p class="text-gray-700 text-xl leading-[28px]">
+              Inventory management, supply chain optimization, task automation
+            </p>
+          </div>
         </div>
       </div>
     </div>
