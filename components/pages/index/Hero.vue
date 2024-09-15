a66b56e892fbe0c0965baebf54fc268e0546df2d
ashrafsamhouri <97393596+ashrafsamhouri@users.noreply.github.com>
2024-09-15 13:05:13 +0300

    white gradient to blend the hero section with the next one

--- components/pages/index/Hero.vue
+++ components/pages/index/Hero.vue
@@ -148,5 +148,18 @@ const { data: homepageAnnouncement } = await useFetch(
   );
   background-size: 600% 600%;
   animation: GradientBackground 10s ease infinite;
+  position: relative;
+  overflow: hidden;
+}
+
+.animated-gradient::after {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 200px;
+  background: linear-gradient(to bottom, transparent, white);
+  pointer-events: none;
 }
 </style>
