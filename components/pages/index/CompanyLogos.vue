3cac51c9e7e75d942847b1f722712bbcfd5d68c8
ashrafsamhouri <97393596+ashrafsamhouri@users.noreply.github.com>
2024-11-04 13:44:01 -0800

    changes to the logos section, startups and fortune 50 and fewer logos and black

--- components/pages/index/CompanyLogos.vue
+++ components/pages/index/CompanyLogos.vue
@@ -1,170 +1,102 @@
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from "vue";
 
-// missing: clickup, contentful, rakuten, roblox, experience.com
-
-const logoSlots = ref([
-  {
-    current: { src: "/logos/posthog.svg" },
-    alternatives: [{ src: "/logos/sequoia.svg" }],
-  },
-  {
-    current: { src: "/logos/redbull.svg" },
-    alternatives: [{ src: "/logos/coherent.svg" }],
-  },
-  {
-    current: { src: "/logos/rakuten.svg" },
-    alternatives: [{ src: "/logos/gratte.svg" }],
-  },
-  {
-    current: { src: "/logos/contentful.svg" },
-    alternatives: [
-      { src: "/logos/formstack.svg" },
-      { src: "/logos/verloop.svg" },
-    ],
-  },
-  {
-    current: { src: "/logos/roblox.svg" },
-    alternatives: [{ src: "/logos/trc.svg" }, { src: "/logos/aeropost.svg" }],
-  },
-  {
-    current: { src: "/logos/ucsd.svg" },
-    alternatives: [{ src: "/logos/ie.svg" }],
-  },
-  {
-    current: { src: "/logos/clickup.svg" },
-    alternatives: [{ src: "/logos/snappy.svg" }],
-  },
-  {
-    current: { src: "/logos/alan.svg" },
-    alternatives: [{ src: "/logos/nedap.svg" }, { src: "/logos/squire.svg" }],
-  },
-  {
-    current: { src: "/logos/docusign.svg" },
-    alternatives: [{ src: "/logos/pipedrive.svg" }],
-  },
-  {
-    current: { src: "/logos/k7security.svg" },
-    alternatives: [{ src: "/logos/pagaleve.svg" }],
-  },
-  {
-    current: { src: "/logos/trustflight.svg" },
-    alternatives: [{ src: "/logos/plivo.svg" }, { src: "/logos/kiel.svg" }],
-  },
-  {
-    current: { src: "/logos/experience.com.svg" },
-    alternatives: [{ src: "/logos/vial.svg" }, { src: "/logos/ipaa.svg" }],
-  },
-]);
-
-let lastIndex1 = null;
-let lastIndex2 = null;
-let intervalId = null;
-
-const getRandomInt = (max, exclude) => {
-  let randomIndex;
-  do {
-    randomIndex = Math.floor(Math.random() * max);
-  } while (randomIndex === exclude);
-  return randomIndex;
-};
-
-const changeLogo = () => {
-  const randomSlotIndex1 = getRandomInt(6, lastIndex1);
-  const slot1 = logoSlots.value[randomSlotIndex1];
-
-  if (!slot1.original) {
-    slot1.original = slot1.current;
-  }
-
-  const currentLogoIndex1 = slot1.alternatives.findIndex(
-    (alt) => alt.src === slot1.current.src
-  );
-  const nextLogoIndex1 =
-    (currentLogoIndex1 + 1) % (slot1.alternatives.length + 1);
-
-  slot1.current =
-    nextLogoIndex1 === slot1.alternatives.length
-      ? slot1.original
-      : slot1.alternatives[nextLogoIndex1];
-
-  lastIndex1 = randomSlotIndex1;
-
-  // Animate one logo from the second 6 positions
-  const randomSlotIndex2 = getRandomInt(6, lastIndex2) + 6;
-  const slot2 = logoSlots.value[randomSlotIndex2];
-
-  if (!slot2.original) {
-    slot2.original = slot2.current; // Store the original logo
-  }
-
-  const currentLogoIndex2 = slot2.alternatives.findIndex(
-    (alt) => alt.src === slot2.current.src
-  );
-  const nextLogoIndex2 =
-    (currentLogoIndex2 + 1) % (slot2.alternatives.length + 1);
-
-  slot2.current =
-    nextLogoIndex2 === slot2.alternatives.length
-      ? slot2.original
-      : slot2.alternatives[nextLogoIndex2];
-
-  lastIndex2 = randomSlotIndex2 - 6;
-};
-
-const startInterval = () => {
-  intervalId = setInterval(changeLogo, 2000);
-};
-
-const stopInterval = () => {
-  clearInterval(intervalId);
-};
-
-const handleVisibilityChange = () => {
-  if (document.hidden) {
-    stopInterval();
-  } else {
-    startInterval();
-  }
-};
-
-onMounted(() => {
-  startInterval();
-  document.addEventListener("visibilitychange", handleVisibilityChange);
-});
-
-onBeforeUnmount(() => {
-  stopInterval();
-  document.removeEventListener("visibilitychange", handleVisibilityChange);
-});
 </script>
 
 <template>
-  <section
-    class="py-[50px] dark:bg-gray-900 max-w-[80%] mx-auto rounded-t-[20px]"
-  >
+  <section class="py-[10px] dark:bg-gray-900 max-w-[80%] mx-auto rounded-t-[20px]">
     <div class="mx-auto max-w-screen-xl px-4">
-      <h2
-        class="mb-14 text-4xl tracking-tight font-bold text-gray-900 dark:text-white text-center"
-      >
-        50,000+ teams are going AI-first
+      <h2 class="mb-14 text-2xl tracking-tight font-bold text-gray-900 dark:text-white text-center">
+        Trusted by startups and Fortune 50 companies
       </h2>
-      <div
-        class="grid grid-cols-1 gap-8 gap-y-14 text-gray-500 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400 opacity-100"
-      >
-        <div
-          v-for="(slot, index) in logoSlots"
-          :key="index"
-          class="relative h-6"
-        >
-          <transition name="fade-slide">
-            <img
-              :src="slot.current.src"
-              :key="slot.current.src"
-              class="absolute top-0 left-0 right-0 bottom-0 m-auto h-6"
-            />
-          </transition>
+      <div class="grid grid-cols-1 gap-20 gap-y-14 text-gray-500 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 dark:text-gray-400 opacity-100 brightness-0">
+        <!-- List of logos displayed directly -->
+        <div class="relative h-6">
+          <img src="/logos/posthog.svg" alt="Posthog" class="m-auto h-6" />
         </div>
+        <div class="relative h-6">
+          <img src="/logos/sequoia.svg" alt="Sequoia" class="m-auto h-6" />
+        </div>
+        <div class="relative h-6">
+          <img src="/logos/redbull.svg" alt="Redbull" class="m-auto h-6" />
+        </div>
+        <!--<div class="relative h-6">
+          <img src="/logos/coherent.svg" alt="Coherent" class="m-auto h-6" />
+        </div>-->
+        <div class="relative h-6">
+          <img src="/logos/rakuten.svg" alt="Rakuten" class="m-auto h-6" />
+        </div>
+        <!--<div class="relative h-6">
+          <img src="/logos/gratte.svg" alt="Gratte" class="m-auto h-6" />
+        </div>-->
+        <div class="relative h-6">
+          <img src="/logos/contentful.svg" alt="Contentful" class="m-auto h-6" />
+        </div>
+        <!--<div class="relative h-6">
+          <img src="/logos/formstack.svg" alt="Formstack" class="m-auto h-6" />
+        </div>-->
+        <!--<div class="relative h-6">
+          <img src="/logos/verloop.svg" alt="Verloop" class="m-auto h-6" />
+        </div>-->
+        <div class="relative h-6">
+          <img src="/logos/roblox.svg" alt="Roblox" class="m-auto h-6" />
+        </div>
+        <!--<div class="relative h-6">
+          <img src="/logos/trc.svg" alt="TRC" class="m-auto h-6" />
+        </div>-->
+        <!--<div class="relative h-6">
+          <img src="/logos/aeropost.svg" alt="Aeropost" class="m-auto h-6" />
+        </div>-->
+        <!--<div class="relative h-6">
+          <img src="/logos/ucsd.svg" alt="UCSD" class="m-auto h-6" />
+        </div>-->
+        <!--<div class="relative h-6">
+          <img src="/logos/ie.svg" alt="IE" class="m-auto h-6" />
+        </div>-->
+        <div class="relative h-6">
+          <img src="/logos/clickup.svg" alt="ClickUp" class="m-auto h-6" />
+        </div>
+        <!--<div class="relative h-6">
+          <img src="/logos/snappy.svg" alt="Snappy" class="m-auto h-6" />
+        </div>-->
+        <div class="relative h-6">
+          <img src="/logos/alan.svg" alt="Alan" class="m-auto h-6" />
+        </div>
+        <!--<div class="relative h-6">
+          <img src="/logos/nedap.svg" alt="Nedap" class="m-auto h-6" />
+        </div>-->
+        <!--<div class="relative h-6">
+          <img src="/logos/squire.svg" alt="Squire" class="m-auto h-6" />
+        </div>-->
+        <div class="relative h-6">
+          <img src="/logos/docusign.svg" alt="DocuSign" class="m-auto h-6" />
+        </div>
+        <div class="relative h-6">
+          <img src="/logos/pipedrive.svg" alt="Pipedrive" class="m-auto h-6" />
+        </div>
+        <!--<div class="relative h-6">
+          <img src="/logos/k7security.svg" alt="K7Security" class="m-auto h-6" />
+        </div>-->
+        <!--<div class="relative h-6">
+          <img src="/logos/pagaleve.svg" alt="Pagaleve" class="m-auto h-6" />
+        </div>-->
+        <!--<div class="relative h-6">
+          <img src="/logos/trustflight.svg" alt="TrustFlight" class="m-auto h-6" />
+        </div>-->
+        <!--<div class="relative h-6">
+          <img src="/logos/plivo.svg" alt="Plivo" class="m-auto h-6" />
+        </div>-->
+        <!--<div class="relative h-6">
+          <img src="/logos/kiel.svg" alt="Kiel" class="m-auto h-6" />
+        </div>-->
+        <!--<div class="relative h-6">
+          <img src="/logos/experience.com.svg" alt="Experience.com" class="m-auto h-6" />
+        </div>-->
+        <!--<div class="relative h-6">
+          <img src="/logos/vial.svg" alt="Vial" class="m-auto h-6" />
+        </div>-->
+        <!--<div class="relative h-6">
+          <img src="/logos/ipaa.svg" alt="IPAA" class="m-auto h-6" />
+        </div>-->
       </div>
     </div>
   </section>
